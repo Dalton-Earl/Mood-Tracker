c2e63eb1278c13215f1110de9ae505754c98b5f8
Dalton <dalto@pop-os.localdomain>
2020-06-02 21:52:56 -0600

    adding boilerplate for mongo db

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const app = express();
+
+const routes = require("./routes")
+
+const PORT = process.env.PORT || 3002
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+app.listen(PORT, function(){
+    console.log (`You are listening on ${PORT} congratulations`);
+});
