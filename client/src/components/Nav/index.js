a944f015993a2ceda64803e11a2840ab1b3beeca
Dalton Earl <Dalton.earl@outlook.com>
2020-06-16 21:23:54 -0600

    Got a basic page working with a nav component that is a place holder the server is working

---
+++ client/src/components/Nav/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <nav className="nav">
+            <a className="nav">
+                Track your mood
+            </a>
+        </nav>
+    )
+}
+
+export default Nav
---
+++ client/src/pages/Moods.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+
+
+function Moods() {
+    return(
+        <div>
+            This is where the moods will be tracked
+        </div>
+    )
+
+}
+
+export default Moods
