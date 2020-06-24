ce7abc4ba9df06c8055af436d0816f6ccf9edc93
Dalton Earl <Dalton.earl@outlook.com>
2020-06-23 20:00:14 -0600

    added the functions to moods page to get and update the moods

--- client/src/pages/Moods.js
+++ client/src/pages/Moods.js
@@ -1,8 +1,49 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import API from "../utils/API";
 
 
 
 function Moods() {
+    const [moods, setMoods] = useState([])
+
+    const [formObject, setFormObject] = useState([])
+
+    useEffect(()=>{
+        loadMoods()
+    }, [])
+
+    function loadMoods(){
+        API.getMoods()
+        .then(res =>
+            setMoods(res.data)
+            )
+            .catch(err => console.log(err));
+
+    };
+
+    function updateMood(id){
+        API.updateMood(id)
+        .then(res => loadMoods())
+        .catch(err => console.log(err));
+    };
+    function handleInputChange(event){
+        const {name, value } = event.target;
+        setFormObject({...formObject, [name]: value})
+    };
+
+    function handleFormSubmit(event){
+        event.preventDefault();
+        if (formObject.mood){
+            API.saveMood({
+                date: formObject.date,
+                mood: formObject.moods
+            })
+            .then(res => loadMoods())
+            .catch(err => console.log(err));
+        }
+    }
+    
+
     return(
         <div>
             This is where the moods will be displayed on charts 
